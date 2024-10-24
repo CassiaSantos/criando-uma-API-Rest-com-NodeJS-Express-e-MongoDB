2b9654e423a4d6fdc1535ce7d32152fbdf9c013b
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-10-23 21:34:46 -0300

    feat: adição de rotas

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 //porta em que o serviador irá executar:
 const PORT = 3000;
 
+//Rota base:
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota de livros",
+    "/autores": "Entrei na rota de autores"
+}
+
 //importação de http de "http" como string - http é uma biblioteca nativa do Node;
 import http from "http";
 
@@ -10,8 +17,8 @@ const server = http.createServer((req, res) => {
     //o cabeçalho conterá o código de status HTTP da requisição e o tipo de conteúdo que irá trafegar
     res.writeHead(200, {"Content-Type": "text/plain"});
     //mesangem transmitida:
-    res.end("Curso de Node.js");
-})
+    res.end(rotas[req.url]);
+});
 
 //método ouvirá o servidor para conexões que acontecerem nele na porta 3000
 server.listen(PORT, () => {
