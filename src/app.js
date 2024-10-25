cb01ff6b0c2575fffe30a992a18f4170d75668d7
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-10-25 20:44:29 -0300

    feat: método HTTP POST para casdatro de livros

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 //importando o express: app conterá todaas as funcionalidades do express
 const app = express();
+//middleware para converter o body da requisição de string em json:
+app.use(express.json())
 
 //Array de livros: 
 const livros = [
@@ -14,6 +16,11 @@ const livros = [
         id: 2,
         titulo: "Harry Potter e a Pedra Filosofal",
         autor: "J. K. Rowling"
+    },
+    {
+        id: 1,
+        titulo: "Luna Clara e Apolo Onze",
+        autor: "Adriana Falcão"
     }
 ]
 //gerenciamento de rotas com express:
@@ -26,6 +33,12 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 });
 
+// ------------------- MÉTODO POST -------------------
+app.post("/livros", (req, res) => {
+    livros.push(req.body)
+    //As requisições precisam de resposta e todas as respostas de sucesso devem conter o status 201
+    res.status(201).send("Livro adicionado com sucesso!")
+})
 
 //Exportando o app para ser usado em outros arquivos:
 export default app;
