f1730819c7d2df88d033b8cf763361f0304620c0
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-10-25 21:00:00 -0300

    feat: GET - recupera livro com id especificado

--- src/app.js
+++ src/app.js
@@ -23,6 +23,13 @@ const livros = [
         autor: "Adriana Falcão"
     }
 ]
+
+//Função para recuperar indice de um livro no array:
+function buscaLivroPorId(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id); //convertendo o id de string para numero
+    })
+}
 //gerenciamento de rotas com express:
 // ------------------- MÉTODO GET -------------------
 app.get("/", (req, res) => {
@@ -33,12 +40,20 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros)
 });
 
+//Recupera livro com id especificado:
+app.get("/livros/:id", (req, res) => {
+    // O id da requisição será passado como parâmetro para a função buscaLivroPorId localizar o livro que tem esse valor como índice no array:
+    const index = buscaLivroPorId(req.params.id);
+    res.status(200).json(livros[index])
+})
+
 // ------------------- MÉTODO POST -------------------
 app.post("/livros", (req, res) => {
     livros.push(req.body)
     //As requisições precisam de resposta e todas as respostas de sucesso devem conter o status 201
     res.status(201).send("Livro adicionado com sucesso!")
 })
+
 
 //Exportando o app para ser usado em outros arquivos:
 export default app;
