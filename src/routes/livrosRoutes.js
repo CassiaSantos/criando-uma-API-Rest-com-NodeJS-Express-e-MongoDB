a621976a3c3396d80fcd3607105024795dac54f8
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-11-01 18:05:15 -0300

    feat: criando rotas  da API e separando código do arquivo app.js

---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import livroController from "../controllers/livroController.js";
+
+const routes = express.Router();
+
+routes.get("/livros", livroController.listarLivros);
