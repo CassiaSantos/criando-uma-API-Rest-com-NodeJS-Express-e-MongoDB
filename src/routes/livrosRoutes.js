5b12fe158c64f282c65319e2ed3ef816050523cd
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-11-01 22:14:36 -0300

    feat: método put com aprimoramento das rotas

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import livroController from "../controllers/livroController.js";
 
-const routesLivro = express.Router();
+const routes = express.Router();
 
-routesLivro.get("/livros", livroController.listarLivros);
+routes.get("/livros", livroController.listarLivros);
+routes.get("/livros/:id", livroController.listarLivrosPorId);
+routes.post("/livros", livroController.cadastrarLivro);
+routes.put("/livros/:id", livroController.atualizarLivro);
+routes.delete("/livros/:id", livroController.deletarLivro);
 
-export default routesLivro;
+export default routes;
