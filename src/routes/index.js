4143b10f7d78f17c810fdecdc0194089018d3ce3
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-11-03 17:23:42 -0300

    feat: integração das rotas de autores no index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@
 
 import express from "express";
 import routes from "./livrosRoutes.js";
+import autores from "./autorRoutes.js";
 
 const routesLivro = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Curso de NodeJS"));
 
     //rotas definidas em routes.js serão passadas para o express:
-    app.use(express.json(), routes);
+    app.use(express.json(), routes, autores);
 };
   
 export default routesLivro; 
