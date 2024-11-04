c031ad3a2ec82d3a067de9df5630a31a65e9e9dc
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-11-04 00:05:00 -0300

    fix: correção de sintaxe

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -1,12 +1,12 @@
 import mongoose, { version } from "mongoose";
 
-const autorSchema = new moongoose.Schema({
+const autorSchema = new mongoose.Schema({
     id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true},
     nome: {type: String, required: true},
     nacionalidade: {type: String}
 },
     {versionKey: false});
 
-const autor = moongoose.model("autores", autorSchema);
+const autor = mongoose.model("autores", autorSchema);
 
-export default {autor, autorSchema}; //autorSchema será exportado pois "um autor faz parte de um livro";
+export {autor, autorSchema}; //autorSchema será exportado pois "um autor faz parte de um livro";
