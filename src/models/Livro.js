c385120007dd5c5a427630a42d2be8360c32b3dd
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-11-03 23:26:40 -0300

    refactor: alteração no model LIvro.js

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,14 +1,14 @@
 import e from "express";
 import mongoose from "mongoose";
-import {autorSchema} from "Autor.js";
+import {autorSchema} from "./Autor.js";
 
 //Schema do livro:
 /*Schema: objeto de configuração que define a estruttura e as propriedades de um documento */
 const livroSchema = new mongoose.Schema({
     id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true},
     titulo: {type: String, required: true},	
-    autor: {type: String, required: true},
     editora: {type: String},
+    preco: {type: Number},
     paginas: {type: Number},
     ano: {type: Number},
     autor: autorSchema
